04a88ead7bf1ac42fa4f9400d52cef815a949783
Christopher Aronson <christopheraronson@icloud.com>
2019-06-23 13:22:55 -0700

    Added Core Data to project.

---
+++ DevelopIt/DevelopIt/Extensions+Utilities/Timer+Convenience.swift
@@ -0,0 +1,21 @@
+//
+//  Timer+Convenience.swift
+//  DevelopIt
+//
+//  Created by Christopher Aronson on 6/23/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Timer {
+    convenience init(title: String, timerLength: Int16, agitateTimer: Int16, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        
+        self.title = title
+        self.timerLength = timerLength
+        self.agitateTimer = agitateTimer
+    }
+}
